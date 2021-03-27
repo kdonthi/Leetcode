111aade65937504d44ae413c7077db162955b48d
Kaushik Donthi <kaushikdr@ucla.edu>
2021-03-26 18:25:44 -0700

    Update reverse_a_linked_list2.py
    Added time complexity and space complexity information for reverse a linked list.

--- LinkedList/reverse_a_linked_list/reverse_a_linked_list2.py
+++ LinkedList/reverse_a_linked_list/reverse_a_linked_list2.py
@@ -9,12 +9,13 @@ class Solution:
             return (head)
         if not head.next:
             return (head)
-        listOfNodes = []
-        ptr = head
+        listOfNodes = [] #n SC
+        ptr = head #1 SC
         while (ptr):
-            listOfNodes.append(ptr)
+            listOfNodes.append(ptr) #n TC
             ptr = ptr.next
         for i in range(len(listOfNodes) - 1, 0, -1): #don't want to set the last node to anything
-            listOfNodes[i].next = listOfNodes[i - 1]
+            listOfNodes[i].next = listOfNodes[i - 1] #n - 1 TC
         listOfNodes[0].next = None
         return(listOfNodes[-1])
+#TOTAL TC: O(n), SC: O(n)
